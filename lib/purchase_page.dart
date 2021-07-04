0afdfb775e61c8438d20eec821fccf40ba30fa3b
hmbl7graze <hmbl7.graze@gmail.com>
2021-07-04 22:33:06 +0900

    購入画面のパディングを調整

--- lib/purchase_page.dart
+++ lib/purchase_page.dart
@@ -13,15 +13,22 @@ class PurchasePage extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const Text(
-                  'プレミアム機能を購入すると下記の機能を使用できます'
-                    '\n・300種以上の季節の御銘の閲覧'
-                    '\n・広告の非表示',
-                  style: TextStyle(fontSize: 15),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                  child: Text(
+                    'プレミアム機能を購入すると下記の機能を使用できます'
+                        '\n・300種以上の季節の御銘の閲覧'
+                        '\n・広告の非表示',
+                    style: TextStyle(fontSize: 15),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('購入する（￥500）'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('購入する（￥500）',
+                      style: TextStyle(fontSize: 18),),
+                  ),
                 )
               ],
             ),
