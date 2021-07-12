01318bb47229807b7bdf9d6fa6eda686c09da69a
hmbl7graze <hmbl7.graze@gmail.com>
2021-07-12 22:48:28 +0900

    購入ページを調整

--- lib/purchase_page.dart
+++ lib/purchase_page.dart
@@ -8,33 +8,68 @@ class PurchasePage extends StatelessWidget {
         title: const Text('購入', style: TextStyle(fontFamily: 'Hannari'),),
       ),
       body: Container(
+        padding: EdgeInsets.all(20),
         child: Center(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                  child: Text(
-                    'プレミアム機能を購入すると下記の機能を使用できます'
-                        '\n・300種以上の季節の御銘の閲覧'
-                        '\n・広告の非表示',
-                    style: TextStyle(fontSize: 15),
-                  ),
+          child: Column(
+            children: [
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                      child: Text(
+                        'プレミアム機能を購入すると下記の機能を使用できます'
+                            '\n・300種以上の季節の御銘の閲覧'
+                            '\n・広告の非表示',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                      child: ElevatedButton(
+                        onPressed: () => onPurchaseButtonClick(context),
+                        child: const Text('購入する(￥300)',
+                          style: TextStyle(fontSize: 21),),
+                      ),
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text('購入する（￥500）',
-                      style: TextStyle(fontSize: 18),),
-                  ),
-                )
-              ],
-            ),
-          ),
+              ),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                      child: Text(
+                        '購入が反映されない場合は下記のボタンで復元します',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                      child: ElevatedButton(
+                        onPressed: () => onRestoreButtonClick(context),
+                        child: const Text('復元',
+                          style: TextStyle(fontSize: 21),),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
         )
       ),
     );
+  }
+
+  void onPurchaseButtonClick(BuildContext context){
+    Navigator.pop(context);
+  }
+
+  void onRestoreButtonClick(BuildContext context){
+    Navigator.pop(context);
   }
 }
